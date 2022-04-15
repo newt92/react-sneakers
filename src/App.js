0329370ffea88aa94116db71ce9e4ed0275152e7
newt92 <tritonan11@gmail.com>
2022-04-15 16:06:53 +0300

    add logic showing  objects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import Card from './components/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
 
+// переменная массива ид объектов
+const arr = [
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg' },
+  {name: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg'},
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
+  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg'},
+];
+
 function App() {
   return (
   <div className="wrapper clear"> 
@@ -18,9 +26,16 @@ function App() {
         </div>
       </div>
 
-
-      <div className="d-flex">
-        <Card />
+    {/* {} - вкл возможность добавить js-код */}
+      <div className="d-flex">        
+        {/* выстраиваем логику отображения объектов */}
+        {arr.map((obj)=>(
+          <Card 
+            title={obj.name} 
+            price={obj.price} 
+            imageUrl={obj.imageUrl} 
+          />
+        ))}
       </div>
 
     </div>
